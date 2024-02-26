4af076db07f0b993760502281097820e9066988b
DonaldPunky <donaldpunkworld@gmail.com>
2024-02-26 10:21:33 -0300

    update 1 file and create 1 file

--- src/App.js
+++ src/App.js
@@ -1,100 +1,53 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Outlet, Route, Routes } from "react-router-dom";
-import { useWeb3React } from "@web3-react/core";
-import "./App.css";
-import Web3ReactManager from "./components/Web3ReactManager";
-import Connection from "./pages/Connection";
-import Footer from "./components/Footer/Footer";
-import Navigation from "./components/Navbar";
-import Account from "./pages/account";
-import Home from "./pages/home.js";
-import Launchpad from "./pages/launchpad.js";
-import LaunchpadInfo from "./pages/launchpadInfo";
-import Locker from "./pages/locker";
-import LockerInfo from "./pages/lockerInfo";
-import LockToken from "./pages/lockToken";
-import Publish from "./pages/publish";
-import { fetchContract } from "./redux/contract/contractAction";
-import * as s from "./styles/global";
-import { useApplicationContext } from "./context/applicationContext";
-import Loader from "./components/Loader";
-import Manage from "./pages/Manage";
+import { useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useWeb3React } from '@web3-react/core';
+import ERC20Balance from "components/ERC20Balance";
+import Home from "containers/home";
+import About from "containers/about";
+import Gallery from "containers/gallery";
+import Transactions from "containers/transactions";
+import NFTs from "containers/nfts";
+import "antd/dist/antd.css";
+import Ramper from "./components/Ramper";
+import Footer from './components/layout/Footer';
+import MainNavigation from "components/layout/Header/MainNavigation";
+import Swap from "containers/swap";
+import Presale from "containers/pre-sale";
+import Mint from "containers/mint";
+import Stake from "containers/stake";
 
-function App() {
-  const dispatch = useDispatch();
-  const { active, chainId, account } = useWeb3React();
-
-  const {
-    isAppConfigured,
-    domainSettings: {
-      networks,
-      contracts,
-      isLockerEnabled
-    },
-    isDomainDataFetching,
-    isDomainDataFetched,
-  } = useApplicationContext();
-
-  useEffect(() => {
-    if (chainId && isAppConfigured) {
-      dispatch(fetchContract(chainId, networks, contracts));
-    }
-  }, [dispatch, account, chainId, isAppConfigured, networks, contracts]);
+const App = () => {
+  const { library, account } = useWeb3React()
+    useEffect(() => {
+      if(library) {
+        localStorage.setItem("connected", true)
+      }
+    }, [library, account]);
 
   return (
-    <Web3ReactManager>
-      <s.Screen>
-        {/* <s.Container ai="center">
-          <div
-            style={{
-              border: "1px solid var(--secondary)",
-              marginBottom: 30,
-              width: "92%",
-            }}
-          />
-        </s.Container> */}
-        {!active ?
-          <Connection />
-          : (isDomainDataFetching || !isDomainDataFetched) ? (
-            <s.LoaderWrapper>
-              <Loader size="2.8rem" />
-            </s.LoaderWrapper>
-            ) : !isAppConfigured ? (
-              <Manage />
-            ) : (
-              <>
-                <Navigation />
-                <s.Container ai="center">
-                  <s.Container w="85%" style={{ minHeight: 600 }}>
-
-                    <Outlet />
-                    <Routes>
-                      <Route path="/" element={<Launchpad />} />
-                      <Route path="/launchpad" element={<Launchpad />} />
-                      <Route
-                        path="/home"
-                        element={<Home />}
-                      />
-                      <Route path="/manage" element={<Manage />} />
-                      <Route path="/launchpad/:idoAddress" element={<LaunchpadInfo />} />
-                      <Route path="/publish" element={<Publish />} />
-                      <Route path="/lock" element={<LockToken />} />
-                      <Route path="/account" element={<Account />} />
-                      { isLockerEnabled && <Route path="/locker" element={<Locker />} /> }
-                      { isLockerEnabled && <Route path="/locker/:lockerAddress" element={<LockerInfo /> } /> }
-                    </Routes>
-                    <s.SpacerLarge />
-                  </s.Container>
-                  <Footer />
-                </s.Container>
-              </>
-            )
-        }
-      </s.Screen>
-    </Web3ReactManager>
+    <BrowserRouter>
+      <MainNavigation />
+      <main style={{marginTop: 90, marginBottom: 90}}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/swap" component={Swap} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/erc20balance" component={ERC20Balance} />
+          <Route exact path="/onramp" component={Ramper} />
+          <Route exact path="/transactions" component={Transactions} />
+          <Route exact path="/nfts" component={NFTs} />
+          <Route exact path="/pre-sale" component={Presale} />
+          <Route exact path="/mint" component={Mint} />
+          <Route exact path="/stake" component={Stake} />
+          <Route exact path="/nonauthenticated">
+            <>Please login using the "Authenticate" button</>
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
