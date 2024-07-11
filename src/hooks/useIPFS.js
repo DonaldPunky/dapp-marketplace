f22212700d705be46ea2ba0961e686a74564cc00
DonaldPunky <donaldpunkworld@gmail.com>
2024-07-11 15:40:32 -0300

    create 9 files and update 1 file

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -1,37 +1,8 @@
-import { useMemo } from 'react';
-import { create } from "ipfs-http-client";
-import { useApplicationContext } from '../context/applicationContext';
-
-// returns null on errors
 export const useIPFS = () => {
+  const resolveLink = (url) => {
+    if (!url || !url.includes("ipfs://")) return url;
+    return url.replace("ipfs://", "https://gateway.ipfs.io/ipfs/");
+  };
 
-  const {
-    domainSettings: {
-      ipfsInfuraProjectId,
-      ipfsInfuraProjectSecret,
-    }
-  } = useApplicationContext();
-
-  const projectId = ipfsInfuraProjectId || process.env.REACT_APP_INFURA_IPFS_KEY;
-  const projectSecret = ipfsInfuraProjectSecret || process.env.REACT_APP_INFURA_IPFS_SECRET;
-
-  return useMemo(() => {
-    if (!projectId || !projectSecret) return null;
-
-    try {
-      const auth = "Basic " + Buffer.from(projectId + ":" + projectSecret).toString('base64');
-
-      return create({
-        host: "ipfs.infura.io",
-        port: 5001,
-        protocol: "https",
-        headers: {
-          authorization: auth,
-        },
-      });
-    } catch (error) {
-      console.error('Failed to get IFPS', error)
-      return null
-    }
-  }, [projectId, projectSecret])
-}
+  return { resolveLink };
+};
