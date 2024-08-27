cd32d6df04ca698247054f43b4f5377d5f950552
DonaldPunky <donaldpunkworld@gmail.com>
2024-08-27 11:30:38 +0900

    update ContractAddress.js, formatters.js and networks.js

--- src/helpers/formatters.js
+++ src/helpers/formatters.js
@@ -1,17 +1,17 @@
-export const n6 = new Intl.NumberFormat("en-us", {
-  style: "decimal",
+export const n6 = new Intl.NumberFormat('en-us', {
+  style: 'decimal',
   minimumFractionDigits: 0,
   maximumFractionDigits: 6,
 });
-export const n4 = new Intl.NumberFormat("en-us", {
-  style: "decimal",
+export const n4 = new Intl.NumberFormat('en-us', {
+  style: 'decimal',
   minimumFractionDigits: 0,
   maximumFractionDigits: 4,
 });
 
-export const c2 = new Intl.NumberFormat("en-us", {
-  style: "currency",
-  currency: "USD",
+export const c2 = new Intl.NumberFormat('en-us', {
+  style: 'currency',
+  currency: 'USD',
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 });
@@ -26,7 +26,7 @@ export const getEllipsisTxt = (str, n = 6) => {
   if (str) {
     return `${str.slice(0, n)}...${str.slice(str.length - n)}`;
   }
-  return "";
+  return '';
 };
 
 export const tokenValue = (value, decimals) => (decimals ? value / Math.pow(10, decimals) : value);
@@ -38,4 +38,5 @@ export const tokenValue = (value, decimals) => (decimals ? value / Math.pow(10, 
  * @param {string} symbol token symbol
  * @returns {string}
  */
-export const tokenValueTxt = (value, decimals, symbol) => `${n4.format(tokenValue(value, decimals))} ${symbol}`;
+export const tokenValueTxt = (value, decimals, symbol) =>
+  `${n4.format(tokenValue(value, decimals))} ${symbol}`;
