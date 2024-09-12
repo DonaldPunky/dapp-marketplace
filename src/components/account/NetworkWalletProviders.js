b292f5cf04b1c48305231da0e16f0f9e58d75815
DonaldPunky <donaldpunkworld@gmail.com>
2024-09-13 02:01:41 +0900

    update FunFactSection.js, NetworkWalletProviders.js and src/components/account/index.js

--- src/components/account/NetworkWalletProviders.js
+++ src/components/account/NetworkWalletProviders.js
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useWeb3React } from '@web3-react/core';
-import { useWalletConnector, setNet } from './WalletConnector.js';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { EthereumLogo, BinanceLogo } from '../ui/NetworkLogos';
-import { MetamaskLogo, WalletConnectLogo } from '../ui/WalletLogos';
-import Avatar from '@mui/material/Avatar';
-import Badge from '@mui/material/Badge';
-import { styled } from '@mui/material/styles';
-import DoneIcon from '@mui/icons-material/Done';
-import { green } from '@mui/material/colors';
+import { useEffect, useState } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { useWalletConnector, setNet } from "./WalletConnector.js";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { EthereumLogo, BinanceLogo } from "../ui/NetworkLogos";
+import { MetamaskLogo, WalletConnectLogo } from "../ui/WalletLogos";
+import Avatar from "@mui/material/Avatar";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import DoneIcon from "@mui/icons-material/Done";
+import { green } from "@mui/material/colors";
 
 const SmallAvatar = styled(Avatar)(({ theme }) => ({
   width: 22,
@@ -27,16 +27,20 @@ const SmallAvatar = styled(Avatar)(({ theme }) => ({
 
 const networks = [
   //{label: "Ethereum", value: "eth", icon: <EthereumLogo width={60} />},
-  { label: 'Binance', value: 'bsc', icon: <BinanceLogo width={60} /> },
+  { label: "Binance", value: "bsc", icon: <BinanceLogo width={60} /> },
 ];
 
 const wallets = [
-  { label: 'Metamask', value: 'injected', icon: <MetamaskLogo width={60} /> },
-  { label: 'Wallet Connect', value: 'walletconnect', icon: <WalletConnectLogo width={60} /> },
+  { label: "Metamask", value: "injected", icon: <MetamaskLogo width={60} /> },
+  {
+    label: "Wallet Connect",
+    value: "walletconnect",
+    icon: <WalletConnectLogo width={60} />,
+  },
 ];
 
 const setWalletProvider = (wallet) => {
-  localStorage.setItem('wallet', wallet);
+  localStorage.setItem("wallet", wallet);
 };
 
 const NetworkWalletProviders = ({
@@ -71,26 +75,26 @@ const NetworkWalletProviders = ({
   };
 
   const connectWallet = async (walletprovider) => {
-    localStorage.setItem('connected', true);
+    localStorage.setItem("connected", true);
 
     switch (walletprovider) {
-      case 'injected_eth':
-        setWalletProvider('injected_eth');
+      case "injected_eth":
+        setWalletProvider("injected_eth");
         setNet(0);
         loginMetamask();
         break;
-      case 'walletconnect_eth':
-        setWalletProvider('walletconnect_eth');
+      case "walletconnect_eth":
+        setWalletProvider("walletconnect_eth");
         setNet(0);
         loginWalletConnect();
         break;
-      case 'injected_bsc':
-        setWalletProvider('injected_bsc');
+      case "injected_bsc":
+        setWalletProvider("injected_bsc");
         setNet(1);
         loginMetamask();
         break;
-      case 'walletconnect_bsc':
-        setWalletProvider('walletconnect_bsc');
+      case "walletconnect_bsc":
+        setWalletProvider("walletconnect_bsc");
         setNet(1);
         loginWalletConnect();
         break;
@@ -100,8 +104,8 @@ const NetworkWalletProviders = ({
   };
 
   useEffect(() => {
-    if (localStorage.getItem('connected')) {
-      connectWallet(localStorage.getItem('wallet'));
+    if (localStorage.getItem("connected")) {
+      connectWallet(localStorage.getItem("wallet"));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -113,10 +117,13 @@ const NetworkWalletProviders = ({
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
       BackdropProps={{
-        style: { backgroundColor: 'rgba(111, 126, 140, 0.2)', backdropFilter: 'blur(2px)' },
+        style: {
+          backgroundColor: "rgba(111, 126, 140, 0.2)",
+          backdropFilter: "blur(2px)",
+        },
       }}
       PaperProps={{
-        style: { borderRadius: 25, boxShadow: 'none' },
+        style: { borderRadius: 25, boxShadow: "none" },
       }}
       fullWidth
       maxWidth="xs"
@@ -132,7 +139,7 @@ const NetworkWalletProviders = ({
             <IconButton
               onClick={handleWalletProvidersDialogToggle}
               aria-label="close"
-              sx={{ bgcolor: 'grey.100' }}
+              sx={{ bgcolor: "grey.100" }}
             >
               <CloseIcon />
             </IconButton>
@@ -141,10 +148,16 @@ const NetworkWalletProviders = ({
       </DialogTitle>
       <DialogContent>
         <Stack direction="row" spacing={2} alignItems="center" mb={2}>
-          <Avatar sx={{ width: 24, height: 24, fontSize: '0.9rem' }}>1</Avatar>
+          <Avatar sx={{ width: 24, height: 24, fontSize: "0.9rem" }}>1</Avatar>
           <Typography sx={{ fontWeight: 500 }}>Choose Network</Typography>
         </Stack>
-        <Stack direction="row" spacing={5} alignItems="center" mb={4} justifyContent="space-evenly">
+        <Stack
+          direction="row"
+          spacing={5}
+          alignItems="center"
+          mb={4}
+          justifyContent="space-evenly"
+        >
           {networks.map((network) => (
             <Stack
               component={Button}
@@ -155,7 +168,7 @@ const NetworkWalletProviders = ({
             >
               <Badge
                 overlap="circular"
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
                 badgeContent={
                   selectedNetwork === network.value ? (
                     <SmallAvatar sx={{ bgcolor: green[500] }}>
@@ -166,17 +179,26 @@ const NetworkWalletProviders = ({
               >
                 <Avatar sx={{ width: 60, height: 60 }}>{network.icon}</Avatar>
               </Badge>
-              <Typography variant="caption" display="block" sx={{ fontWeight: 500 }}>
+              <Typography
+                variant="caption"
+                display="block"
+                sx={{ fontWeight: 500 }}
+              >
                 {network.label}
               </Typography>
             </Stack>
           ))}
         </Stack>
         <Stack direction="row" spacing={2} alignItems="center" mb={2}>
-          <Avatar sx={{ width: 24, height: 24, fontSize: '0.9rem' }}>2</Avatar>
+          <Avatar sx={{ width: 24, height: 24, fontSize: "0.9rem" }}>2</Avatar>
           <Typography sx={{ fontWeight: 500 }}>Choose Wallet</Typography>
         </Stack>
-        <Stack direction="row" spacing={3} alignItems="center" justifyContent="space-evenly">
+        <Stack
+          direction="row"
+          spacing={3}
+          alignItems="center"
+          justifyContent="space-evenly"
+        >
           {wallets.map((wallet) => (
             <Stack
               component={Button}
@@ -187,7 +209,7 @@ const NetworkWalletProviders = ({
             >
               <Badge
                 overlap="circular"
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
                 badgeContent={
                   selectedWallet === wallet.value ? (
                     <SmallAvatar sx={{ bgcolor: green[500] }}>
@@ -198,7 +220,11 @@ const NetworkWalletProviders = ({
               >
                 <Avatar sx={{ width: 60, height: 60 }}>{wallet.icon}</Avatar>
               </Badge>
-              <Typography variant="caption" display="block" sx={{ fontWeight: 500 }}>
+              <Typography
+                variant="caption"
+                display="block"
+                sx={{ fontWeight: 500 }}
+              >
                 {wallet.label}
               </Typography>
             </Stack>
--- src/components/account/index.js
+++ src/components/account/index.js
@@ -1,6 +1,6 @@
-import { useWeb3React } from '@web3-react/core';
-import Authenticated from './Authenticated';
-import Unauthenticated from './Unauthenticated';
+import { useWeb3React } from "@web3-react/core";
+import Authenticated from "./Authenticated";
+import Unauthenticated from "./Unauthenticated";
 
 function Account() {
   const { library, account } = useWeb3React();
