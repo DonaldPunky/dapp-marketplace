cf939b5d933bba126e325a412c933a132932e8f4
DonaldPunky <donaldpunkworld@gmail.com>
2024-09-20 11:06:31 +0900

    update buyTokenCard.js, longLocker.js, poolRenderer.js and Unauthenticated.js

--- src/components/Card/longLocker.js
+++ src/components/Card/longLocker.js
@@ -1,28 +1,38 @@
-import BigNumber from 'bignumber.js';
-import React from 'react';
-import Countdown from 'react-countdown';
-import { NavLink } from 'react-router-dom';
-import { usePoolContext } from '../../context/poolContext';
-import * as s from '../../styles/global';
+import BigNumber from "bignumber.js";
+import React from "react";
+import Countdown from "react-countdown";
+import { NavLink } from "react-router-dom";
+import { usePoolContext } from "../../context/poolContext";
+import * as s from "../../styles/global";
 
 const LongLocker = (props) => {
   const { lockerAddress } = props;
   const lockerInfo = usePoolContext().allLocker[lockerAddress];
 
   if (!lockerInfo) {
-    return <s.Card ai="center" style={{ maxWidth: 500, margin: 20, minWidth: 400 }}></s.Card>;
+    return (
+      <s.Card
+        ai="center"
+        style={{ maxWidth: 500, margin: 20, minWidth: 400 }}
+      ></s.Card>
+    );
   }
 
   return (
     <NavLink
-      to={'/locker/' + lockerAddress}
+      to={"/locker/" + lockerAddress}
       style={{
-        textDecoration: 'none',
-        color: 'white',
-        width: '100%',
+        textDecoration: "none",
+        color: "white",
+        width: "100%",
       }}
     >
-      <s.Card fd="row" p={'20px'} style={{ maxWidth: '100%' }} jc="space-between">
+      <s.Card
+        fd="row"
+        p={"20px"}
+        style={{ maxWidth: "100%" }}
+        jc="space-between"
+      >
         <s.Container flex={1}>
           <s.TextDescription>{lockerInfo.name}</s.TextDescription>
           <s.TextID>{lockerInfo.token.tokenSymbol}</s.TextID>
@@ -30,13 +40,15 @@ const LongLocker = (props) => {
         <s.Container flex={1} ai="flex-end">
           <s.TextDescription>
             {BigNumber(lockerInfo.balance)
-              .dividedBy(BigNumber(10 ** parseInt(lockerInfo.token.tokenDecimals)))
+              .dividedBy(
+                BigNumber(10 ** parseInt(lockerInfo.token.tokenDecimals)),
+              )
               .toFixed(2) +
-              ' $' +
+              " $" +
               lockerInfo.token.tokenSymbol}
           </s.TextDescription>
           {BigNumber(lockerInfo.time).lt(Date.now() / 1000) ? (
-            <s.TextID style={{ color: 'var(--primary)' }}>UNLOCKED</s.TextID>
+            <s.TextID style={{ color: "var(--primary)" }}>UNLOCKED</s.TextID>
           ) : (
             <s.TextID>
               <Countdown date={parseInt(lockerInfo.time) * 1000}></Countdown>
--- src/components/Card/poolRenderer.js
+++ src/components/Card/poolRenderer.js
@@ -1,22 +1,32 @@
-import BigNumber from 'bignumber.js';
-import React, { useEffect, useRef, useState } from 'react';
-import { Badge } from 'react-bootstrap';
-import Countdown from 'react-countdown';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { getValidImageUrl } from '../../utils/utils';
-import * as s from '../../styles/global';
-import ProgressBar from '../Modal/ProgressBar';
+import BigNumber from "bignumber.js";
+import React, { useEffect, useRef, useState } from "react";
+import { Badge } from "react-bootstrap";
+import Countdown from "react-countdown";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getValidImageUrl } from "../../utils/utils";
+import * as s from "../../styles/global";
+import ProgressBar from "../Modal/ProgressBar";
 
-import imageSolid from '../../assets/images/image-solid.png';
-import { useApplicationContext } from '../../context/applicationContext';
+import imageSolid from "../../assets/images/image-solid.png";
+import { useApplicationContext } from "../../context/applicationContext";
 
 const PoolRenderer = (props) => {
   const contract = useSelector((state) => state.contract);
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
   const {
     pool: idoInfo,
-    pool: { start, end, metadata, idoAddress, tokenName, tokenSymbol, softCap, hardCap, progress },
+    pool: {
+      start,
+      end,
+      metadata,
+      idoAddress,
+      tokenName,
+      tokenSymbol,
+      softCap,
+      hardCap,
+      progress,
+    },
   } = props;
 
   const {
@@ -34,11 +44,16 @@ const PoolRenderer = (props) => {
       setImage(
         getValidImageUrl(
           idoInfo?.metadata?.image || idoInfo?.metadata?.imageHash,
-          ipfsInfuraDedicatedGateway
-        )
+          ipfsInfuraDedicatedGateway,
+        ),
       );
     }
-  }, [idoInfo, idoInfo.metadata.image, idoInfo.metadata.imageHash, ipfsInfuraDedicatedGateway]);
+  }, [
+    idoInfo,
+    idoInfo.metadata.image,
+    idoInfo.metadata.imageHash,
+    ipfsInfuraDedicatedGateway,
+  ]);
 
   // if (!utils.isValidPool(idoInfo) || !idoInfo) {
   //   return (
@@ -56,11 +71,11 @@ const PoolRenderer = (props) => {
   return (
     <s.Card ref={card} style={{ maxWidth: 500, margin: 20, minWidth: 400 }}>
       <NavLink
-        to={'/launchpad/' + idoAddress}
+        to={"/launchpad/" + idoAddress}
         style={{
-          textDecoration: 'none',
-          color: 'white',
-          width: '100%',
+          textDecoration: "none",
+          color: "white",
+          width: "100%",
         }}
       >
         <s.UpperCard fd="row" jc="space-between" ai="center">
@@ -75,7 +90,10 @@ const PoolRenderer = (props) => {
           </s.Container>
           <s.SpacerSmall />
           <s.Container flex={3} ai="center" style={{ paddingLeft: 5 }}>
-            <s.TextDescriptionEllipsis style={{ textAlign: 'center' }} fs={'26px'}>
+            <s.TextDescriptionEllipsis
+              style={{ textAlign: "center" }}
+              fs={"26px"}
+            >
               {tokenName}
             </s.TextDescriptionEllipsis>
             <s.TextID>${tokenSymbol}</s.TextID>
@@ -101,18 +119,24 @@ const PoolRenderer = (props) => {
         <s.Container fd="row">
           <s.Container ai="center" flex={1}>
             <s.TextID fullWidth>Soft cap</s.TextID>
-            {BigNumber(contract.web3.utils.fromWei(softCap)).toFormat(2) + ' ' + baseCurrencySymbol}
+            {BigNumber(contract.web3.utils.fromWei(softCap)).toFormat(2) +
+              " " +
+              baseCurrencySymbol}
           </s.Container>
           <s.Container ai="center" flex={1}>
             <s.TextID fullWidth>Hard cap</s.TextID>
-            {BigNumber(contract.web3.utils.fromWei(hardCap)).toFormat(2) + ' ' + baseCurrencySymbol}
+            {BigNumber(contract.web3.utils.fromWei(hardCap)).toFormat(2) +
+              " " +
+              baseCurrencySymbol}
           </s.Container>
         </s.Container>
         <s.SpacerSmall />
         {!hasEnded && (
           <>
-            <s.TextID>{isStarted ? 'End in' : 'Start in'}</s.TextID>
-            <Countdown date={isStarted ? parseInt(end) * 1000 : parseInt(start) * 1000} />
+            <s.TextID>{isStarted ? "End in" : "Start in"}</s.TextID>
+            <Countdown
+              date={isStarted ? parseInt(end) * 1000 : parseInt(start) * 1000}
+            />
           </>
         )}
         <s.TextID>Progress</s.TextID>
